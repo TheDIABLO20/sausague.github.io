1a97a552f6c3f9218e768014c67e93572f1258d5
TheDIABLO20 <64171730+TheDIABLO20@users.noreply.github.com>
2026-07-18 09:06:03 -0500

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,84 @@
+import { createClient }
+from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const supabaseUrl =
+"https://TU-PROYECTO.supabase.co";
+
+const supabaseKey =
+"TU_ANON_KEY";
+
+const supabase = createClient(
+    supabaseUrl,
+    supabaseKey
+);
+
+let registerMode = false;
+
+const title = document.getElementById("title");
+const actionBtn = document.getElementById("actionBtn");
+const switchMode = document.getElementById("switchMode");
+const message = document.getElementById("message");
+
+switchMode.addEventListener("click", () => {
+
+    registerMode = !registerMode;
+
+    title.textContent = registerMode
+        ? "Crear Cuenta"
+        : "Iniciar Sesión";
+
+    actionBtn.textContent = registerMode
+        ? "Registrarme"
+        : "Iniciar Sesión";
+
+    switchMode.textContent = registerMode
+        ? "¿Ya tienes cuenta? Inicia sesión"
+        : "¿No tienes cuenta? Regístrate";
+});
+
+actionBtn.addEventListener("click", async () => {
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+
+        if(registerMode){
+
+            const { error } =
+            await supabase.auth.signUp({
+                email,
+                password
+            });
+
+            if(error) throw error;
+
+            message.innerHTML =
+            "✅ Cuenta creada correctamente.";
+
+        }else{
+
+            const { error } =
+            await supabase.auth.signInWithPassword({
+                email,
+                password
+            });
+
+            if(error) throw error;
+
+            message.innerHTML =
+            "🎉 Bienvenido.";
+
+            setTimeout(() => {
+                window.location.href =
+                "dashboard.html";
+            }, 1000);
+        }
+
+    } catch(error){
+
+        message.innerHTML =
+        `❌ ${error.message}`;
+    }
+
+});
